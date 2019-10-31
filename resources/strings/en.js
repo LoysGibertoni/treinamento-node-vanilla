8e1d446f1ca8f3d2cb285e49a48e5c9ff7e81a57
Loys <loys.gibertoni@zarpsystem.com.br>
2019-10-31 10:00:50 -0300

    Congelar recursos string

--- resources/strings/en.js
+++ resources/strings/en.js
@@ -1,5 +1,5 @@
-module.exports = {
+module.exports = Object.freeze({
     CREATE_SUCCESS: 'Record successfully created!',
     UPDATE_SUCCESS: 'Record successfully updated!',
     DELETE_SUCCESS: 'Record successfully deleted!'
-};
+});
--- resources/strings/pt-BR.js
+++ resources/strings/pt-BR.js
@@ -1,5 +1,5 @@
-module.exports = {
+module.exports = Object.freeze({
     CREATE_SUCCESS: 'Registro criado com sucesso!',
     UPDATE_SUCCESS: 'Registro atualizado com sucesso!',
     DELETE_SUCCESS: 'Registro apagado com sucesso!'
-};
+});
